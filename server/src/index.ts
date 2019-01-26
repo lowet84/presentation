c3ecb383d10177225d70b9b0775eca5df4a68e7b
Fredrik Löwenhamn <fredrik.lowenhamn@gmail.com>
2019-01-26 08:32:55 +0100

    Wip

--- server/src/index.ts
+++ server/src/index.ts
@@ -6,9 +6,9 @@ import { routingControllersToSpec } from 'routing-controllers-openapi'
 import * as express from 'express'
 import { TestController } from './controllers/TestController'
 import 'reflect-metadata'
-import { writeFileSync } from 'fs'
+import { writeFileSync, existsSync, mkdirSync } from 'fs'
 const compression = require('compression')
-var Converter = require('api-spec-converter');
+var Converter = require('api-spec-converter')
 
 var dev = process.argv[2] === 'dev'
 
@@ -25,15 +25,19 @@ app.use(express.static('web'))
 if (dev) {
   const storage = getMetadataArgsStorage()
   const spec = routingControllersToSpec(storage)
+  
+  if (!existsSync('web')) {
+    mkdirSync('web')
+  }
+  writeFileSync('web/openApi.json', JSON.stringify(spec), 'utf8')
   Converter.convert({
     from: 'openapi_3',
     to: 'swagger_2',
-    source: 'https://api.gettyimages.com/swagger/api-docs',
+    source: 'http://localhost:3000/openApi.json',
   })
-  .then(function(converted) {
-    console.log(converted.stringify());
+  .then(function(converted: any) {
+    writeFileSync('../client/openApi.json',converted.stringify(),'utf8');
   });
-  writeFileSync('../client/openApi.json',JSON.stringify(spec),'utf8')
 }
 
 app.listen(3000)
