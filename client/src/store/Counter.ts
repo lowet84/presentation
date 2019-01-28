e03b4f8cf274da516829aa267e5ab7d752e6207a
Fredrik Löwenhamn <fredrik.lowenhamn@gmail.com>
2019-01-28 16:59:35 +0100

    Added sample code

--- client/src/store/Counter.ts
+++ client/src/store/Counter.ts
@@ -1,22 +1,27 @@
-import {Module, VuexModule, Mutation, Action} from 'vuex-module-decorators' 
+import { Module, VuexModule, Mutation, Action } from 'vuex-module-decorators'
 
-@Module({namespaced: true})
+@Module({ namespaced: true })
 export default class Counter2 extends VuexModule {
   count = 0
 
   @Mutation increment(delta: number) {
-    this.count+=delta
+    this.count += delta
   }
   @Mutation decrement(delta: number) {
-    this.count-=delta
+    this.count -= delta
   }
 
   // action 'incr' commits mutation 'increment' when done with return value as payload
-  @Action({commit: 'increment'}) incr() {return 5}
+  @Action({ commit: 'increment' }) incr() {
+    console.log(`going from ${this.count} to ${this.count + 5}`)
+    return 5
+  }
   // action 'decr' commits mutation 'decrement' when done with return value as payload
-  @Action({commit: 'decrement'}) decr() {return 5}
+  @Action({ commit: 'decrement' }) decr() {
+    return 5
+  }
 
-  get value(){
+  get value() {
     return this.count
   }
 }
