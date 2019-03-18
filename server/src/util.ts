96bf6c17a800c0b4da644fd34ec74a90051fccb9
Fredrik Löwenhamn <lowet84@gmail.com>
2019-03-18 21:51:37 +0100

    Wip testing stuff

--- server/src/util.ts
+++ server/src/util.ts
@@ -2,7 +2,7 @@ var fileLines = (value: string, fileName: string): string => {
   var lines = value.split(/\r?\n/).filter(d => d.trim().length > 0)
   var ret = ''
   lines.forEach((line, index) => {
-    ret += `echo ${line} ${index === 0 ? '>' : '>>'} ${fileName} ${
+    ret += `echo "${line}" ${index === 0 ? '>' : '>>'} ${fileName} ${
       index === lines.length - 1 ? '' : '&& '
     }`
   })
